b21bed269c7ae4a31853a03f2ff00caba02c1e1a
Timothy Van Heest <timothy@ionicsecurity.com>
2017-01-02 16:15:06 -0500

    Fixes to scraping script

--- scrape/process_chapter.py
+++ scrape/process_chapter.py
@@ -27,12 +27,16 @@ def process_chapter(book, chapter, rel_url):
 
     # http://pyquery.readthedocs.io/en/latest/api.html#pyquery.pyquery.PyQuery.remove
     # sup.versenum, sup.crossreference, sup.footnote
-    verses("table.passage-cols td.passage-col div.passage-text p > span.text sup").remove()
-    verses("table.passage-cols td.passage-col div.passage-text p > span.text span.chapternum").remove()
+    vstxt = verses("table.passage-cols td.passage-col div.passage-text")
+    vstxt.find("p span.text sup").remove()
+    vstxt.find("table span.text sup").remove()
+    vstxt.find("p span.text span.chapternum").remove()
 
-    vss = verses("table.passage-cols td.passage-col div.passage-text p > span.text")
+    # Maybe only grabbing one...?
+    vss = vstxt.find("p span.text, table span.text")
 
     prev_verse = 0
+    prev_line = ""
 
     for vs in vss:
         txt = vs.text_content().strip()
@@ -40,16 +44,28 @@ def process_chapter(book, chapter, rel_url):
         grp = re.search("[a-zA-z]+\-(\d+)\-(\d+)", vs.attrib['class'])
         chapter, verse = grp.group(1), grp.group(2)
 
+        # DEBUG
+        #print(chapter, verse, txt)
+
         if prev_verse == verse:
-            # check for continuation
-            line = u'%s %s' % (line, txt)
+            # Continuation; append
+            prev_line = u'%s %s' % (prev_line, txt)
             continue
 
+        # Yield the line from the previous iteration
+        if prev_line != "":
+            yield prev_line
+
+        # Form the new line
         line = u'%s\t%s\t%s\t%s\t%s' % (version, book, chapter, verse, txt)
         line.encode('utf8')
-        yield line
 
+        # Update tracking of previous line
+        prev_line = line
         prev_verse = verse
+
+    # Last line
+    yield prev_line
 
 
 if __name__ == "__main__":
